244e0386eb52b1339283a53254a2711916aaaf9f
Thomas Klijnsma <klthomas@student.ethz.ch>
2019-04-01 16:14:46 +0200

    added projections paths module

---
+++ projections_paths.py
@@ -0,0 +1,13 @@
+from differentials.core import AttrDict
+
+pt_cards = AttrDict()
+pt_cards.hgg         = 'suppliedInput/fromVittorio/pT_newBins_Feb28/projection_hgg_s2groups_Jul17.txt'
+pt_cards.hzz         = 'suppliedInput/fromDavid/PTH_Jan24_newBinning/smH/projection_hzz_s2groups_Jul17.txt'
+pt_cards.hbb         = 'suppliedInput/fromJavier/bernstein_r7428/projection_hbb_s2groups_Jul17.txt'
+pt_cards.combWithHbb = 'suppliedInput/projection_combWithHbb_s2groups_Jul17.txt'
+
+pt_cards_nongrouped = AttrDict()
+pt_cards_nongrouped.hgg = 'suppliedInput/fromVittorio/pT_newBins_Feb28/projection_hgg_Jun28.txt'
+pt_cards_nongrouped.hzz = 'suppliedInput/fromDavid/PTH_Jan24_newBinning/smH/projection_hzz_Jun28.txt'
+pt_cards_nongrouped.hbb = 'suppliedInput/fromJavier/bernstein_r7428/projection_hbb_Jun28.txt'
+pt_cards_nongrouped.combWithHbb = 'suppliedInput/projection_combWithHbb_Jun28.txt'
